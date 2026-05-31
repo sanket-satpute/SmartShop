f5b06299f8d48ebf8355c72325650529025400f6
sanket satpute <86184107+sanket-satpute@users.noreply.github.com>
2026-05-31 12:22:14 +0530

    added API Gateway single entry point for all the services

---
+++ ApiGateway/ApiGateway/src/test/java/com/smart_shop/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_shop.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
